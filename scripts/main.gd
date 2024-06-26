7488f880ac8bc885ca14e1ea348c619adbaf9470
Sergey Bubnov <omgbebebe@gmail.com>
2024-06-26 19:37:53 +0400

    initial radial pupup menu mockup

--- scripts/main.gd
+++ scripts/main.gd
@@ -19,4 +19,6 @@ func _process(delta: float) -> void:
 	get_window().size = Vector2i(width, height)
 
 func _on_hud_switch():
+	# TODO: hide and show to bring the window
+	# to the active workspace in case of tiling WM
 	$main_ui_window.visible = !$main_ui_window.visible
