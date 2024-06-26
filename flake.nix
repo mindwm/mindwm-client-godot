2a198bf8217fc0da1b41688a6c6ea4165295a72b
Sergey Bubnov <omgbebebe@gmail.com>
2024-06-26 14:13:23 +0400

    add devshell

--- flake.nix
+++ flake.nix
@@ -4,13 +4,16 @@
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/24.05";
     flake-parts.url = "github:hercules-ci/flake-parts";
+    devshell.url = "github:numtide/devshell";
     gdextension_xorg.url = "github:mindwm/gdextension-xorg";
     gdextension_dbus.url = "github:mindwm/gdextension-dbus";
   };
 
   outputs = inputs@{ flake-parts, gdextension_xorg, gdextension_dbus, ... }:
     flake-parts.lib.mkFlake { inherit inputs; } {
-      imports = [];
+      imports = [
+        inputs.devshell.flakeModule
+      ];
       systems = [ "x86_64-linux" "aarch64-linux" ];
       perSystem = { config, self', inputs', pkgs, system, ... }:
         let
@@ -18,13 +21,28 @@
             {
               ${name} = value.packages.${system};
             }) (pkgs.lib.attrsets.filterAttrs (n: v: pkgs.lib.strings.hasPrefix "gdextension_" n) inputs);
-        in { packages = rec {
-          release = pkgs.callPackage ./package.nix { inherit gdexts; withTarget = "template_release"; };
-          debug = pkgs.callPackage ./package.nix { inherit gdexts; withTarget = "template_debug"; };
-          default = release;
+        in {
+          packages = rec {
+            release = pkgs.callPackage ./package.nix { inherit gdexts; withTarget = "template_release"; };
+            debug = pkgs.callPackage ./package.nix { inherit gdexts; withTarget = "template_debug"; };
+            default = release;
+          };
+        
+          devshells.default = {
+            env = [
+              { name = "HTTP_PORT"; value = "8080"; }
+            ];
+            commands = [
+            { help = "update gdextensions";
+              name = "update";
+              command = "mkdir -p ./addons && cp -asv ${config.packages.debug}/addons/* ./addons/";
+            }
+            ];
+            packages = [
+              config.packages.debug
+            ];
+          };
         };
-        devShells.default = config.packages.debug;
-      };
       flake = {
       };
     };
