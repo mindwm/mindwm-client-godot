31aa331dc84bfca395b7ec662a44871f3e287f6a
Sergey Bubnov <omgbebebe@gmail.com>
2024-06-26 19:14:04 +0400

    hide root window

--- scripts/main.gd
+++ scripts/main.gd
@@ -1,15 +1,22 @@
 extends Node2D
 
+@export var width : int = 0
+@export var height : int = 0
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
+	# set root window min.size to zero
+	get_tree().get_root().set_min_size(Vector2i(0,0))
+	
+	# set initiali size
+	get_window().size = Vector2i(width, height)
+	
 	Global.dbus.hud_switch.connect(_on_hud_switch)
-	pass # Replace with function body.
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	pass
+	get_window().size = Vector2i(width, height)
 
 func _on_hud_switch():
 	$main_ui_window.visible = !$main_ui_window.visible
