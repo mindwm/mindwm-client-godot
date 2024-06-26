6918879cf0848ef0850ca16f676b8179d110b586
Sergey Bubnov <omgbebebe@gmail.com>
2024-06-26 18:51:40 +0400

    pin flake inputs packages

--- flake.nix
+++ flake.nix
@@ -2,12 +2,19 @@
   description = "MindWM Client";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/24.05";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-24.05";
     flake-parts.url = "github:hercules-ci/flake-parts";
+    flake-parts.inputs.nixpkgs.follows = "nixpkgs";
     devshell.url = "github:numtide/devshell";
     godot.url = "github:mindwm/godot4-nix";
+    godot.inputs.nixpkgs.follows = "nixpkgs";
+    godot-cpp.url = "github:omgbebebe/godot-cpp";
     gdextension_xorg.url = "github:mindwm/gdextension-xorg";
+    gdextension_xorg.inputs.nixpkgs.follows = "nixpkgs";
+    gdextension_xorg.inputs.godot-cpp.follows = "godot-cpp";
     gdextension_dbus.url = "github:mindwm/gdextension-dbus";
+    gdextension_dbus.inputs.nixpkgs.follows = "nixpkgs";
+    gdextension_dbus.inputs.godot-cpp.follows = "godot-cpp";
   };
 
   outputs = inputs@{ flake-parts, gdextension_xorg, gdextension_dbus, ... }:
